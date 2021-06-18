2e01a5f63184d78ad25399d9ecbbdd3ee154f609
jeffssant <santana.jeff@gmail.com>
2021-06-18 19:42:20 -0300

    gitignore*

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: 'https://gateway.marvel.com/v1/public/characters',
-  apiKey: '17ac01275f6b1b6a4e3e14310ff4a64d',
-  hash: '9f1f88b814c24997d1b837c9d7f76cb10796435e',
-};
