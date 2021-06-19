cd019125dedc80fbb6f6c7f6b1396d4395ccba4e
jeffssant <santana.jeff@gmail.com>
2021-06-19 13:09:36 -0300

    Esrtilizando os cards / Capturando erros

--- src/app/heroes/home/home.component.ts
+++ src/app/heroes/home/home.component.ts
@@ -1,34 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
 import { GetHeroesService } from '../service/get-heros.service';
 
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  templateUrl: './home.component.html'  
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterContentChecked {
 
   heroes = ['hulk', 'Spider-Man', 'wolverine'];
-  detalheHeroes: any = [];
+  detailHero: any = [];
+  
+  erros:number = 0
 
-  constructor(private getHeros: GetHeroesService) { }
+  constructor(private getHeros: GetHeroesService) {  }
 
   ngOnInit(): void {
-    this.heroes.map((heroe) => {
-      
+    
+    this.heroes.map((heroe) => {    
+
       this.getHeros.getHeroesId(heroe).subscribe(resp => { 
         
-        var thumbnail = `${resp.thumbnail.path}/portrait_uncanny.${resp.thumbnail.extension}`
-        this.detalheHeroes.push({'name': resp.name, 'stories': resp.stories.items,'thumbnail': thumbnail})
+        var thumbnail = `${resp.thumbnail.path}/standard_fantastic.${resp.thumbnail.extension}`
+        this.detailHero.push({'name': resp.name, 'stories': resp.stories.items.slice(0, 5),'thumbnail': thumbnail})
 
       })
       
     });
-
   }
 
-  
-
+  ngAfterContentChecked(): void {
+    this.erros = this.getHeros.erros;
+  }
 
 }
--- src/app/heroes/service/get-heros.service.ts
+++ src/app/heroes/service/get-heros.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import {Md5} from 'ts-md5/dist/md5';
-import { map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { Heroes } from "../../../app/heroes/home/interfaces/home.interface";
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -18,17 +19,22 @@ export class GetHeroesService {
   private hash = this.md5.appendStr(this.hashDate+environment.hash+environment.apiKey).end();
   private query = `ts=${this.hashDate}&apikey=${environment.apiKey}&hash=${this.hash}&limit=1`
 
+  public erros:number = 0; 
+  
   constructor(private http: HttpClient) { }
   
   getHeroesId(heroName: string) {
 
     return this.http.get<Heroes>(`${this.baseUrl}?name=${heroName}&${this.query}`)
-    .pipe( map(
-              resp => { 
-                return resp.data.results[0]
-              }
-            )
-          );
+    .pipe( 
+
+      catchError(err => {      
+        this.erros++;   
+        return throwError(err);
+      }),
+
+      map(resp => {return resp.data.results[0]})
+    );
   }
 
 }
